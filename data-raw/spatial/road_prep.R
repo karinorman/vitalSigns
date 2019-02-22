8e1c7419fabdddbec7f5accc77c16a0e903d5370
katherinesiegel <katherinesiegel@github.com>
2019-02-21 19:02:18 -0800

    code to extract roads in VS countries from gRoads

---
+++ data-raw/spatial/road_prep.R
@@ -0,0 +1,41 @@
+### Code to get roads for the 4 countries
+
+### Load libraries
+library(tidyverse)
+library(sf)
+library(raster)
+library(rworldmap)
+
+### Set paths
+## source('src/CalcRdsStats.R')
+data.path <- '~/Dropbox/vitalSigns/data/spatial/'
+save.path <- '~/Dropbox/vitalSigns/saved/spatial/'
+
+### Read in road data
+# roads <- readOGR(dsn = "C:/Users/Katherine Siegel/Dropbox/vitalSigns/data/spatial/roads/gROADS-v1-africa-shp",
+#                  layer = "gROADS-v1-africa")
+
+#### Save road data to dropbox
+## save(roads, file = file.path(save.path, "roads.Rdata"))
+
+### Load road data - we did this because the shp of African roads took so long to load
+load("~/Dropbox/vitalSigns/saved/spatial/roads.Rdata")
+roads <- st_as_sf(roads)
+
+### Get country maps from rworldmaps-- used this to crop the roads data 
+data(countriesLow)
+country_map <- countriesLow
+
+### Subset Vital Signs countries
+vs_countries <- country_map[country_map@data$ISO3 %in% c("RWA",
+                                                         "TZA", "UGA",
+                                                         "GHA"),] %>% st_as_sf()
+
+### Put vs_countries in same projection as roads
+vs_countries <-  st_transform(vs_countries, crs(roads))
+
+### Extract roads in VS countries
+vs_roads <- st_intersection(roads, vs_countries)
+
+### Write sf of roads
+st_write(vs_roads, file.path(data.path, "vs_roads.shp"))
